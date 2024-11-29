489e7a4b284f1a589963ac0a63f352124ee1f433
Urian Viera <urian1213viera@gmail.com>
2024-11-29 11:52:11 -0500

    cambiando el modo debug

--- project_core/settings.py
+++ project_core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^*8f#rid-deoiyxh6*3@(udins6e4gubf3=vh!ztyo4iavg3qb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
