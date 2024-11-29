7e2d7becbfde5d1e7cb69a1eed927d031d7d8749
Urian Viera <urian1213viera@gmail.com>
2024-11-29 14:20:52 -0500

    ajustes demo

--- project_core/urls.py
+++ project_core/urls.py
@@ -25,6 +25,4 @@ urlpatterns = [
     path('', include('portafolio.urls')),
 ]
 
-
-if settings.DEBUG:
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
